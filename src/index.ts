42c669948222d5a3f51977d937d1ac48716f7900
nsnhatlong <nsnl.coder@gmail.com>
2023-04-25 01:42:20 -0400

    💚 fix cicd bug

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env.dev' });
-// dotenv.config({ path: '.env.prod' });
 //
 import db from './config/db';
 import { app } from './config/app';
 
 db();
+
+dotenv.config({ path: '.env' });
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.prod' });
+} else {
+  dotenv.config({ path: '.env.dev' });
+}
 
 const PORT = process.env.PORT || 8080;
 
